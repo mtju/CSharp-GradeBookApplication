adba6e9b51f57bd0ce937f138b681c4c2b39fae6
Motiejus Eringis <motiejus.eringis@gmail.com>
2018-10-01 21:45:25 +0300

    Module 3.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,27 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            if (PercentOfStudentsWithWorseAverageGrade(averageGrade) >= 80) return 'A';
+            if (PercentOfStudentsWithWorseAverageGrade(averageGrade) >= 60) return 'B';
+            if (PercentOfStudentsWithWorseAverageGrade(averageGrade) >= 40) return 'C';
+            if (PercentOfStudentsWithWorseAverageGrade(averageGrade) >= 20) return 'D';
+            // No E
+            return 'F';
+        }
+
+        private double PercentOfStudentsWithWorseAverageGrade(double averageGrade)
+        {
+            var countOfStudentsWithWorseAverageGrade = Students.Where(x => x.AverageGrade < averageGrade).Count();
+            return (double)countOfStudentsWithWorseAverageGrade / (double)Students.Count * 100;
         }
     }
 }
